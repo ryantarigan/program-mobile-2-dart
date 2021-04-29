17214ee5bda89cee980e0168c387a57c1ecb138a
ryantarigan <78521977+ryantarigan@users.noreply.github.com>
2021-04-29 13:58:16 +0700

    Create MahasiswaModel.dart

---
+++ MahasiswaModel.dart
@@ -0,0 +1,14 @@
+AtletVolly.dart
+class MahasiswaModel {
+  MahasiswaModel({
+    this.no,
+    this.name,
+    this.position,
+    this.power,
+  });
+
+  String no;
+  String name;
+  String position;
+  int power;
+}
